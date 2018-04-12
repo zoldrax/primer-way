b334a4a5b48f14db2fe3381a0b69a393e46e221e
zoldrax <28215900+zoldrax@users.noreply.github.com>
2018-04-12 16:57:50 +0300

    refactoring

--- primerway.py
+++ primerway.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+import ConfigParser
 import getopt
 import gzip
+import json
 import math
 import os
 import re
@@ -10,7 +12,6 @@ import sys
 import primer3
 import pysam
 import vcf
-
 
 def get_pseudo_qualities_from_VCF(VCF_file, target_region):
     pseudo_qualities = []
@@ -38,33 +39,12 @@ def get_candidate_pairs(sequence, left_edge, right_edge, step):
         'SEQUENCE_ID': 'example',
         'SEQUENCE_TEMPLATE': ''
     }
-    p3prim = dict(
-        PRIMER_QUALITY_RANGE_MAX=40,
-        PRIMER_WT_SEQ_QUAL=0.03,
-        PRIMER_WT_END_QUAL=0.1,
-        PRIMER_PICK_LEFT_PRIMER=1,
-        PRIMER_PICK_RIGHT_PRIMER=1,
-        PRIMER_MAX_END_GC=3,
-        PRIMER_MAX_POLY_X=4,
-        PRIMER_PRODUCT_SIZE_RANGE=[[50, 300]],
-        PRIMER_MAX_TM=65,
-        PRIMER_WT_SIZE_LT=0.5,
-        PRIMER_WT_SIZE_GT=0.5,
-        PRIMER_WT_TM_LT=0.1,
-        PRIMER_WT_TM_HT=0.05,
-        PRIMER_PAIR_WT_DIFF_TM=0.3,
-        PRIMER_WT_HAIRPIN_TH=2,
-        PRIMER_WT_SELF_ANY_TH=1,
-        PRIMER_WT_SELF_END_TH=2,
-        PRIMER_PAIR_WT_COMPL_ANY_TH=1,
-        RIMER_PAIR_WT_COMPL_END_TH=2,
-        PRIMER_EXPLAIN_FLAG=1,
-        PRIMER_NUM_RETURN=per_target
-    )
+    p3prim = dict([(x[0].upper(), json.loads(x[1])) for x in config.items("Primer3")])
+
     for i in range(left_edge, right_edge, step):
         target_length = right_edge - i
-        if target_length > max_target_length:
-            target_length = max_target_length
+        if target_length > step:
+            target_length = step
 
         p3seq['SEQUENCE_TEMPLATE'] = sequence
 
@@ -78,7 +58,8 @@ def get_candidate_pairs(sequence, left_edge, right_edge, step):
             if deletion == "":
                 p3seq['SEQUENCE_TARGET'] = [i, target_length]
             else:
-                p3seq['SEQUENCE_TARGET'] = [(flanking + 20), 2]
+                p3seq['SEQUENCE_TARGET'] = [(config.getint("PrimerWay", "flanking") +
+                                             config.getint("PrimerWay", "min_overlap")), 2]
         else:
             if exon < 2:
                 p3seq['SEQUENCE_FORCE_LEFT_END'] = 50
@@ -87,10 +68,10 @@ def get_candidate_pairs(sequence, left_edge, right_edge, step):
 
         result = primer3.bindings.designPrimers(p3seq, p3prim)
 
-        for i in xrange(per_target):
+        for i in xrange(p3prim['PRIMER_NUM_RETURN']):
             si = str(i)
             spair = (
-            result.get('PRIMER_LEFT_' + si + '_SEQUENCE', ''), result.get('PRIMER_RIGHT_' + si + '_SEQUENCE', ''))
+                result.get('PRIMER_LEFT_' + si + '_SEQUENCE', ''), result.get('PRIMER_RIGHT_' + si + '_SEQUENCE', ''))
             if spair[0] != '':
                 candidate_pairs[spair] = [
                     result['PRIMER_LEFT_' + si][0] + result['PRIMER_LEFT_' + si][1] + 1,
@@ -112,12 +93,13 @@ def add_penalty_for_non_specific_pairs(candidate_pairs):
         i += 1
         complete += 1
         s += "_".join(pair) + "\t" + pair[0] + "\t" + pair[1] + "\n"
-        if (i >= per_tntblast) | (pair == candidate_pairs.keys()[-1]):
+        if (i >= config.get("tntBLAST", "pairs_per_run")) | (pair == candidate_pairs.keys()[-1]):
             if verbose: print s
             print ("Tntblast: " + str(complete) + " of " + str(len(candidate_pairs)))
-            proc = subprocess.Popen((
-                                            "nice -1 tntblast -i /dev/stdin -d " + reference_file + " -g -5 -l 1000 --temperature 330 --primer-clamp 1").split(),
-                                    stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+            proc = subprocess.Popen(
+                (config.get("tntBLAST", "run_command") + " -i /dev/stdin -d " + reference_file).split(),
+                stdout=subprocess.PIPE, stdin=subprocess.PIPE
+            )
             proc.stdin.write(s)
             s = ""
             i = 0
@@ -127,10 +109,10 @@ def add_penalty_for_non_specific_pairs(candidate_pairs):
             pcrs = re.findall("name = (\w+)", result)
             for pcr in pcrs:
                 candidate_pairs[tuple(pcr.split('_'))][3] += 1
-                candidate_pairs[tuple(pcr.split('_'))][2] += penalty_PCR_product
+                candidate_pairs[tuple(pcr.split('_'))][2] += config.getfloat("PrimerWay", "penalty_PCR_product")
     return candidate_pairs
 
-def get_the_best_way (penalised_pairs, left_edge, right_edge):
+def get_the_best_way(penalised_pairs, left_edge, right_edge):
     graph = {"START": []}
     for pair1 in penalised_pairs:
         if penalised_pairs[pair1][0] < left_edge: graph["START"].append(pair1)
@@ -138,44 +120,37 @@ def get_the_best_way (penalised_pairs, left_edge, right_edge):
             graph[pair1] = graph.get(pair1, [])
             graph[pair1].append("END")
         for pair2 in penalised_pairs:
-            if penalised_pairs[pair2][0] < (penalised_pairs[pair1][1] - overlap):
+            if penalised_pairs[pair2][0] < (penalised_pairs[pair1][1] - config.getint("PrimerWay", "min_overlap")):
                 graph[pair1] = graph.get(pair1, [])
                 graph[pair1].append(pair2)
 
     penalised_pairs["END"] = [0, 0, 0, 0]
-    Visited = {}
+    visited = {}
     to_visit = {"START": 0}
-    Paths = {"START": ["START"]}
+    paths = {"START": ["START"]}
     while to_visit:
         v = min(to_visit, key=lambda x: to_visit[x])
-        Visited[v] = to_visit[v]
+        visited[v] = to_visit[v]
         del to_visit[v]
         for w in graph.get(v, []):
-            if w not in Visited:
-                vwLength = Visited[v] + ((penalised_pairs[w][2] - penalty_PCR_product) ** 2) + penalty_base_level
+            if w not in visited:
+                vwLength = visited[v] + (
+                        (penalised_pairs[w][2] - config.getfloat("PrimerWay", "penalty_PCR_product")) ** 2) \
+                           + config.getfloat("PrimerWay", "base_penalty_for_pair")
                 if (w not in to_visit) or (vwLength < to_visit[w]):
                     to_visit[w] = vwLength
-                    Paths[w] = Paths[v] + [w]
+                    paths[w] = paths[v] + [w]
     try:
-        best_way = Paths["END"][1:-1]
+        the_best_way = paths["END"][1:-1]
     except:
-        best_way = []
-    return best_way
+        the_best_way = []
+    return the_best_way
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hvG:R:V:i:o:p:r:a:n:s:d:")
+    opts, args = getopt.getopt(sys.argv[1:], 'hvG:R:V:i:o:p:r:a:n:s:d:c:')
 except:
-    pass
-
+    opts, args = [], []
 text_width = 60
-max_target_length = 50
-flanking = 300
-overlap = 20
-penalty_PCR_product = 100
-penalty_base_level = 3
-per_target = 300
-per_tntblast = 600
-
 verbose = False
 job_name = "Exon"
 protein_id = ""
@@ -184,6 +159,7 @@ deletion = ""
 as_coord = ""
 output_d = ""
 input_d = ""
+config_file = "primerway.cfg"
 reference_file = ""
 reference_gff_file = ""
 variant_file = ""
@@ -207,6 +183,7 @@ for o, a in opts:
         print "-a : target nucleotide coordinate for allele specific (example: 11:121212)"
         print "-n : prefix for primer names"
         print "-s : start from exon number (default 1)"
+        print "-c : set configuration file (default primerway.cfg)"
         sys.exit()
 
     elif o in "-s":
@@ -224,6 +201,8 @@ for o, a in opts:
         as_coord = a
     elif o in "-o":
         output_d = a
+    elif o in "-c":
+        config_file = a
     elif o in "-i":
         input_d = a
     elif o in "-R":
@@ -234,6 +213,9 @@ for o, a in opts:
         variant_file = a
     elif o in "-n":
         job_name = a
+
+config = ConfigParser.ConfigParser()
+config.read(config_file)
 
 if output_d == "":
     print "Error: Output directory is needed"
@@ -277,10 +259,19 @@ if (deletion != "") & (reference_file != ""):
         except:
             start = 0
             end = 0
-        target_regions.append([fh.fetch(
-            region=regreg[0][0] + ":" + str(start - flanking - 21) + "-" + str(start - 1)) + fh.fetch(
-            region=regreg[0][0] + ":" + str(end + 1) + "-" + str(end + flanking + 21)), regreg[0][0],
-                               range(start - flanking - 21, start) + range(end + 1, end + flanking + 22)])
+        target_regions.append([
+            fh.fetch(region=regreg[0][0] + ":" +
+                            str(start - config.getint("PrimerWay", "flanking") - config.getint("PrimerWay",
+                                                                                               "min_overlap") - 1) +
+                            "-" + str(start - 1)) +
+            fh.fetch(region=regreg[0][0] + ":" +
+                            str(end + 1) + "-" + str(end + config.getint("PrimerWay", "flanking") +
+                                                     config.getint("PrimerWay", "min_overlap") + 1)),
+            regreg[0][0], range(start - config.getint("PrimerWay", "flanking") -
+                                config.getint("PrimerWay", "min_overlap") - 1, start) +
+            range(end + 1, end + config.getint("PrimerWay", "flanking") +
+                  config.getint("PrimerWay", "min_overlap") + 2)
+        ])
     protein_id = ""
     user_region = ""
 
@@ -289,8 +280,9 @@ if (user_region != "") & (reference_file != ""):
     regreg = re.findall("(.+):(\d+)-(\d+)", user_region)
     with pysam.FastaFile(reference_file) as fh:
         try:
-            start = int(regreg[0][1]) - flanking - 20
-            end = int(regreg[0][2]) + flanking + 20
+            start = int(regreg[0][1]) - config.getint("PrimerWay", "flanking") - config.getint("PrimerWay",
+                                                                                               "min_overlap")
+            end = int(regreg[0][2]) + config.getint("PrimerWay", "flanking") + config.getint("PrimerWay", "min_overlap")
         except:
             start = 0
             end = 0
@@ -304,13 +296,14 @@ if (protein_id != "") & (reference_gff_file != ""):
         exons = re.findall("(chr[\dXY]+)\t.+\tCDS\t(\d+)\t(\d+)\t.+protein_id=" + protein_id, csvfile.read())
     print ("Found " + str(len(exons)) + " exons.")
     fex = open(output_d + "/Exon_list.txt", 'w')
-    fex.write("Coord without flanking " + str(flanking + 20) + "\n")
+    fex.write("Coord without flanking " + str(config.getint("PrimerWay", "flanking") +
+                                              config.getint("PrimerWay", "min_overlap")) + "\n")
     for i in exons:
         fex.write("\t".join(i) + "\n")
         with pysam.FastaFile(reference_file) as fh:
             try:
-                start = int(i[1]) - flanking - 20
-                end = int(i[2]) + flanking + 20
+                start = int(i[1]) - config.getint("PrimerWay", "flanking") - config.getint("PrimerWay", "min_overlap")
+                end = int(i[2]) + config.getint("PrimerWay", "flanking") + config.getint("PrimerWay", "min_overlap")
             except:
                 start = 0
                 end = 0
@@ -333,12 +326,12 @@ for target_region in target_regions:
 
     l_quality = get_pseudo_qualities_from_VCF(variant_file, target_region)
 
-    right_edge = len(sequence) - flanking
-    left_edge = flanking
+    right_edge = len(sequence) - config.getint("PrimerWay", "flanking")
+    left_edge = config.getint("PrimerWay", "flanking")
     if as_coord != "":
         right_edge = left_edge + 1
 
-    pairs = get_candidate_pairs(sequence, left_edge, right_edge, 50)
+    pairs = get_candidate_pairs(sequence, left_edge, right_edge, config.getint("PrimerWay", "Primer3_step"))
 
     print ("primers: " + str(len(pairs)))
 
@@ -350,7 +343,7 @@ for target_region in target_regions:
 
     for pair in pairs:
         if (pairs[pair][3] == 0) & (deletion == ""):
-            pairs[pair][2] += 2 * penalty_PCR_product
+            pairs[pair][2] += 2 * config.getfloat("PrimerWay", "penalty_PCR_product")
 
     sorted_pairs = sorted(pairs, key=lambda d: pairs[d][2])
     for pair in sorted_pairs:
@@ -367,14 +360,14 @@ for target_region in target_regions:
     penalty = 0
     prpair = 0
     specchar = dict()
-    specchar[left_edge + 20] = "{"
-    specchar[right_edge - 20] = "}"
+    specchar[left_edge + config.getint("PrimerWay", "min_overlap")] = "{"
+    specchar[right_edge - config.getint("PrimerWay", "min_overlap")] = "}"
     for pair in best_way:
         prpair += 1
         if deletion != "":
             penalty += (pairs[pair][2] - 0) ** 2
         else:
-            penalty += (pairs[pair][2] - penalty_PCR_product) ** 2
+            penalty += (pairs[pair][2] - config.getfloat("PrimerWay", "penalty_PCR_product")) ** 2
         f.write(pair[0] + "\t" + pair[1] + "\t" + "\t" + str(pairs[pair][0]) + "\t" + str(pairs[pair][1]) + "\t" + str(
             pairs[pair][2]) + "\n")
         f.flush()
